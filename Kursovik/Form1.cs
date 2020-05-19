7c2ccd1be1f32a4c6533eda2a8ea7c514d389aae
Alexander Vasiliev <smash3243@gmail.com>
2020-05-19 14:29:56 +0300

    Init

---
+++ Kursovik/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace Kursovik
+{
+    public partial class Form1 : Form
+    {
+        DataTable dt = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK) {
+                textBox1.Text = file.FileName;
+                string[] lines = File.ReadAllLines(file.FileName);
+                string[] values;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    values = lines[i].ToString().Split(';');
+                    string[] row = new string[values.Length];
+                    for (int j = 0; j < values.Length; j++)
+                    {
+                        row[j] = values[j].Trim();
+                    }
+                    dt.Rows.Add(row);
+
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dt.Columns.Add("x");
+            dt.Columns.Add("y");
+            dt.Columns.Add("x2");
+            dt.Columns.Add("y2");
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
